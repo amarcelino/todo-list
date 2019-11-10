4412ffc936e06d5f820b31c65006091aaeed8e66
Andre Marcelino <marcelinoandre@gmail.com>
2019-11-10 03:21:24 -0300

    v1 90% concuida

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import uuidv4 from 'uuid/v4'
 import './App.css'
+// import TodoList from './components/todolist'
 
 const App = () => {
+  const [todo, setTodo] = useState('')
+  const [done, setDone] = useState(false)
+  const [todoList, setTodoList] = useState([])
+
+  const handleAddTodo = (e) => {
+    e.preventDefault()
+    if (!todo) return
+    setTodoList([...todoList, { todo, done, id: uuidv4() }])
+    setTodo('')
+  }
   return (
-    <div className='App' />
+
+    <div>
+      {console.log(todoList)}
+      <h1>Todo List</h1>
+      <form onSubmit={handleAddTodo}>
+        <input
+          type='text'
+          value={todo}
+          placeholder='Digite ToDo here...'
+          onChange={e => setTodo(e.target.value)}
+        />
+        <button>Add </button>
+      </form>
+
+      <ul>
+        {
+          todoList.map(e => (
+            <li key={e.id}>{e.todo}</li>
+          ))
+        }
+      </ul>
+
+    </div>
   )
 }
 
---
+++ src/components/todolist.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoList = () => (
+  <div>
+    <h1>Todo List</h1>
+    <input type='text' placeholder='Add To do' />
+    <button>Add </button>
+
+    <ul />
+  </div>
+)
+
+export default TodoList
